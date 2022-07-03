f67ec8c03ad8e0a15ae7500b70764f24d110b0b3
Gerald Spencer <gerald@skydio.com>
2022-07-03 14:30:36 -0700

    Initial commit of LXStudio IDE
    I often get tired of bootstrapping Slee's LX project. Let's address that with:
    
    - git submodudle -> LXStudio-IDE
    - gradle to stitch the submodule together with a new project

---
+++ src/main/java/com/follybuilders/paradisium/ParadisiumApp.java
@@ -0,0 +1,183 @@
+/* (C)2020-2022 */
+package com.follybuilders.paradisium;
+
+import heronarts.lx.LX;
+import heronarts.lx.LXComponent;
+import heronarts.lx.LXPlugin;
+import heronarts.lx.osc.LXOscComponent;
+import heronarts.lx.parameter.BoundedParameter;
+import heronarts.lx.studio.LXStudio;
+import heronarts.p4lx.ui.component.UICollapsibleSection;
+import heronarts.p4lx.ui.component.UIKnob;
+import java.io.File;
+import processing.core.PApplet;
+
+/**
+ * This is an example top-level class to build and run an LX Studio application via an IDE. The
+ * main() method of this class can be invoked with arguments to either run with a full Processing 3
+ * UI or as a headless command-line only engine.
+ */
+public class ParadisiumApp extends PApplet implements LXPlugin {
+
+  private static final String WINDOW_TITLE = "LX Studio";
+
+  private static int WIDTH = 1280;
+  private static int HEIGHT = 800;
+  private static boolean FULLSCREEN = false;
+
+  private static int WINDOW_X = 0;
+  private static int WINDOW_Y = 0;
+
+  private static boolean HAS_WINDOW_POSITION = false;
+
+  @Override
+  public void settings() {
+    if (FULLSCREEN) {
+      fullScreen(PApplet.P3D);
+    } else {
+      size(WIDTH, HEIGHT, PApplet.P3D);
+    }
+    pixelDensity(displayDensity());
+  }
+
+  @Override
+  public void setup() {
+    LXStudio.Flags flags = new LXStudio.Flags(this);
+    flags.resizable = true;
+    flags.useGLPointCloud = false;
+    flags.startMultiThreaded = true;
+
+    new LXStudio(this, flags);
+    this.surface.setTitle(WINDOW_TITLE);
+    if (!FULLSCREEN && HAS_WINDOW_POSITION) {
+      this.surface.setLocation(WINDOW_X, WINDOW_Y);
+    }
+  }
+
+  public static class MyComponent extends LXComponent implements LXOscComponent {
+
+    public final BoundedParameter param1 =
+        new BoundedParameter("p1", 0).setDescription("A global parameter that does something");
+
+    public final BoundedParameter param2 =
+        new BoundedParameter("p2", 0).setDescription("A global parameter that does something else");
+
+    public MyComponent(LX lx) {
+      super(lx);
+      addParameter("param1", this.param1);
+      addParameter("param2", this.param2);
+    }
+  }
+
+  // A global component for additional project-specific parameters, if desired
+  public MyComponent myComponent;
+
+  @Override
+  public void initialize(LX lx) {
+    // Here is where you should register any custom components or make modifications
+    // to the LX engine or hierarchy. This is also used in headless mode, so note that
+    // you cannot assume you are working with an LXStudio class or that any UI will be
+    // available.
+
+    // Register custom pattern and effect types
+
+    // Create an instance of your global component and register it with the LX engine
+    // so that it can be saved and loaded in project files
+    this.myComponent = new MyComponent(lx);
+    lx.engine.registerComponent("myComponent", this.myComponent);
+  }
+
+  public void initializeUI(LXStudio lx, LXStudio.UI ui) {
+    // Here is where you may modify the initial settings of the UI before it is fully
+    // built. Note that this will not be called in headless mode. Anything required
+    // for headless mode should go in the raw initialize method above.
+  }
+
+  public static class UIMyComponent extends UICollapsibleSection {
+    public UIMyComponent(LXStudio.UI ui, MyComponent myComponent) {
+      super(ui, 0, 0, ui.leftPane.global.getContentWidth(), 80);
+      setTitle("MY COMPONENT");
+
+      new UIKnob(0, 0, myComponent.param1).addToContainer(this);
+      new UIKnob(40, 0, myComponent.param2).addToContainer(this);
+    }
+  }
+
+  public void onUIReady(LXStudio lx, LXStudio.UI ui) {
+    // At this point, the LX Studio application UI has been built. You may now add
+    // additional views and components to the UI hierarchy.
+    new UIMyComponent(ui, this.myComponent).addToContainer(ui.leftPane.global);
+  }
+
+  @Override
+  public void draw() {
+    // All handled by core LX engine, do not modify, method exists only so that Processing
+    // will run a draw-loop.
+  }
+
+  /**
+   * Main interface into the program. Two modes are supported, if the --headless flag is supplied
+   * then a raw CLI version of LX is used. If not, then we embed in a Processing 4 applet and run as
+   * such.
+   *
+   * @param args Command-line arguments
+   */
+  public static void main(String[] args) {
+    LX.log("Initializing LX version " + LXStudio.VERSION);
+    boolean headless = false;
+    File projectFile = null;
+    for (int i = 0; i < args.length; ++i) {
+      if ("--help".equals(args[i])) {
+      } else if ("--headless".equals(args[i])) {
+        headless = true;
+      } else if ("--fullscreen".equals(args[i]) || "-f".equals(args[i])) {
+        FULLSCREEN = true;
+      } else if ("--width".equals(args[i]) || "-w".equals(args[i])) {
+        try {
+          WIDTH = Integer.parseInt(args[++i]);
+        } catch (Exception x) {
+          LX.error("Width command-line argument must be followed by integer");
+        }
+      } else if ("--height".equals(args[i]) || "-h".equals(args[i])) {
+        try {
+          HEIGHT = Integer.parseInt(args[++i]);
+        } catch (Exception x) {
+          LX.error("Height command-line argument must be followed by integer");
+        }
+      } else if ("--windowx".equals(args[i]) || "-x".equals(args[i])) {
+        try {
+          WINDOW_X = Integer.parseInt(args[++i]);
+          HAS_WINDOW_POSITION = true;
+        } catch (Exception x) {
+          LX.error("Window X command-line argument must be followed by integer");
+        }
+      } else if ("--windowy".equals(args[i]) || "-y".equals(args[i])) {
+        try {
+          WINDOW_Y = Integer.parseInt(args[++i]);
+          HAS_WINDOW_POSITION = true;
+        } catch (Exception x) {
+          LX.error("Window Y command-line argument must be followed by integer");
+        }
+      } else if (args[i].endsWith(".lxp")) {
+        try {
+          projectFile = new File(args[i]);
+        } catch (Exception x) {
+          LX.error(x, "Command-line project file path invalid: " + args[i]);
+        }
+      }
+    }
+    if (headless) {
+      // We're not actually going to run this as a PApplet, but we need to explicitly
+      // construct and set the initialize callback so that any custom components
+      // will be run
+      LX.Flags flags = new LX.Flags();
+      flags.initialize = new ParadisiumApp();
+      if (projectFile == null) {
+        LX.log("WARNING: No project filename was specified for headless mode!");
+      }
+      LX.headless(flags, projectFile);
+    } else {
+      PApplet.main("com.follybuilders.paradisium.ParadisiumApp", args);
+    }
+  }
+}
