3d5d84b298409f2f6d8f1853e7b618ee1e14f3a3
Gerald H Spencer <ger.spencer3@gmail.com>
2022-07-05 11:36:08 -0700

    feature: Init testing framework
    First pass, let's use junit-jupiter to ensure that we can start
    ParadisiumApp in headless mode.

---
+++ src/test/java/com/follybuilders/paradisium/ParadisiumAppTest.java
@@ -0,0 +1,15 @@
+/* (C)2022 */
+package com.follybuilders.paradisium;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import heronarts.lx.LX;
+import org.junit.jupiter.api.Test;
+
+class ParadisiumAppTest {
+  @Test
+  void setup() {
+    LX.Flags flags = ParadisiumApp.headlessInit(null);
+    flags.initialize.initialize(new LX());
+  }
+}
