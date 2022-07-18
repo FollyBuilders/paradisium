111c7dd667f5e5b41dce8a807bb532ab116a135b
John Parts Taylor <parts@LumiGeek.com>
2022-07-18 14:14:26 -0700

    Fixed tag error in Isolation patterns and created a simple project file to show A/B use of multi-channel DMX fixtures.

--- src/main/java/com/follybuilders/paradisium/pattern/AmberPattern.java
+++ src/main/java/com/follybuilders/paradisium/pattern/AmberPattern.java
@@ -8,12 +8,12 @@ import heronarts.lx.LXCategory;
 import heronarts.lx.model.LXModel;
 
 @LXCategory(ParadisiumCategory.RGBWAUVPATTERN)
-public class AmberPattern extends LXPattern {
+public class AmberPattern extends ParadisiumBasePattern {
 
     public AmberPattern(LX lx) {super(lx);}
 
     public void run(double deltaMs) {
-        String tag = "AMBER";
+        String tag = "WHITE";
         for (LXModel strip : model.sub(tag)) {
             setColor(strip, 0xff00ff00);
         }
