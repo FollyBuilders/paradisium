3c14e1a839b49cdd12b1792e26d04ed27e7a3c81
John Parts Taylor <parts@LumiGeek.com>
2022-07-18 12:06:44 -0700

    Added a category for RGB+W+A+UV to group the base patterns together in LX.

---
+++ src/main/java/com/follybuilders/paradisium/pattern/RGBPattern.java
@@ -0,0 +1,23 @@
+/* (C)2022 */
+package com.follybuilders.paradisium.pattern.test;
+
+import com.follybuilders.paradisium.ParadisiumCategory;
+import com.follybuilders.paradisium.pattern.ParadisiumBasePattern;
+import heronarts.lx.LX;
+import heronarts.lx.LXCategory;
+import heronarts.lx.model.LXModel;
+
+@LXCategory(ParadisiumCategory.RGBWAUVPATTERN)
+public class RGBPattern extends ParadisiumBasePattern {
+
+  public RGBPattern(LX lx) {
+    super(lx);
+  }
+
+  public void run(double deltaMs) {
+    String tag = "RGB";
+    for (LXModel strip : model.sub(tag)) {
+      setColor(strip, 0xffffffff);
+    }
+  }
+}
