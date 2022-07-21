593138f5d921e73672e3d4dcc3df80790b7cc2d5
John Parts Taylor <parts@LumiGeek.com>
2022-07-21 16:34:29 -0700

    Updated LXF files to reflect new 3DM. Added latest OBJ.  Dropped in support for OBJ props in global tab of LX.

---
+++ src/main/java/com/follybuilders/paradisium/props/UIPropList.java
@@ -0,0 +1,40 @@
+package com.follybuilders.paradisium.props;
+
+import heronarts.lx.LX;
+import heronarts.lx.studio.LXStudio;
+import heronarts.p4lx.P4LX;
+import heronarts.p4lx.ui.component.UIButton;
+import heronarts.p4lx.ui.component.UICollapsibleSection;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UIPropList extends UICollapsibleSection {
+
+    List<UIProp> props = new ArrayList<UIProp>();
+
+    public UIPropList(P4LX lx, LXStudio.UI ui, float w, List<File> objs, VisibilityParameterHelper vh) {
+        super(ui, 0, 0, w, 32);
+        setTitle("OBJ PROPS");
+        if (objs != null) {
+            for (int i = 0; i < objs.size(); i++) {
+                File f = objs.get(i);
+                LX.log("Adding prop for file: " + f.getName());
+                UIProp p = new UIProp(lx, f);
+                props.add(p);
+                ui.preview.addComponent(p);
+                new UIButton(0, i * 18, w - 8, 16)
+                        .setParameter(p.visible)
+                        .setLabel(p.name())
+                        .setActiveColor(ui.theme.getSecondaryColor())
+                        .addToContainer(this);
+                vh.attachParameterForFilename(f.getName(),p.visible);
+
+            }
+
+
+            setContentHeight(18 * objs.size());
+        }
+    }
+}
---
+++ src/main/java/com/follybuilders/paradisium/props/VisibilityParameterHelper.java
@@ -0,0 +1,48 @@
+package com.follybuilders.paradisium.props;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import heronarts.lx.LX;
+import heronarts.lx.LXComponent;
+import heronarts.lx.LXSerializable;
+import heronarts.lx.parameter.BooleanParameter;
+import heronarts.lx.parameter.LXParameter;
+import heronarts.lx.parameter.LXParameterListener;
+
+import java.io.File;
+import java.util.*;
+
+public class VisibilityParameterHelper extends LXComponent implements LXSerializable {
+
+    ArrayList<File> filenames = new ArrayList<File>();
+    HashMap<String,BooleanParameter> parameters = new HashMap<String,BooleanParameter>();
+
+    public void addFoundFiles(List<File> f) {
+        filenames.addAll(f);
+    }
+
+    public void attachParameterForFilename(String fn,BooleanParameter vp) {
+        parameters.put(fn,vp);
+    }
+
+    public void load(LX lx, JsonObject j) {
+        //LX.log("INSIDE VISIBILITY HELPER LOAD(): " + j.toString());
+        for (Map.Entry<String,JsonElement> entry : j.entrySet()) {
+            String k = entry.getKey();
+            BooleanParameter b = parameters.get(k);
+            if (b != null) {
+                b.setValue(entry.getValue().getAsBoolean());
+            }
+        }
+    }
+
+    public void save(LX lx, JsonObject j) {
+        for (Map.Entry<String,BooleanParameter> entry : parameters.entrySet()) {
+            j.addProperty(entry.getKey(),entry.getValue().getValueb());
+        }
+        //LX.log("INSIDE VISIBILITY HELPER SAVE() " + j.toString());
+    }
+
+
+
+}
