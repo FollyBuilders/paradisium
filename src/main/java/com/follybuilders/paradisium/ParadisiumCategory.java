ee57deab985ddfd0629641c8a17350082f0cdc0a
Gerald Spencer <ger.spencer3@gmail.com>
2022-08-26 22:26:52 -0700

    WAUV weren't working for gradients..
    It was trying to use all of the LEDs.. as the WAUV fixtures were secretly RGB in the model. Shrug

--- src/main/java/com/follybuilders/paradisium/ParadisiumCategory.java
+++ src/main/java/com/follybuilders/paradisium/ParadisiumCategory.java
@@ -4,4 +4,5 @@ package com.follybuilders.paradisium;
 public interface ParadisiumCategory {
   String TESTPATTERN = "Test Patterns";
   String RGBWAUVPATTERN = "RGB+W+A+UV";
+  String GERALD = "Gerald";
 }
--- src/main/java/com/follybuilders/paradisium/pattern/gerald/Random.java
+++ src/main/java/com/follybuilders/paradisium/pattern/gerald/Random.java
@@ -1,5 +1,22 @@
 package com.follybuilders.paradisium.pattern.gerald;
 
-public class Random {
-    
+import com.follybuilders.paradisium.ParadisiumCategory;
+import com.follybuilders.paradisium.pattern.ParadisiumBasePattern;
+
+import heronarts.lx.LX;
+import heronarts.lx.LXCategory;
+import heronarts.lx.parameter.BoundedParameter;
+
+@LXCategory(ParadisiumCategory.GERALD)
+public class Random extends ParadisiumBasePattern {
+    BoundedParameter red = new BoundedParameter("Red", 0, 255);
+    BoundedParameter green = new BoundedParameter("Red", 0, 255);
+    BoundedParameter blue = new BoundedParameter("Red", 0, 255);
+    public Random(LX lx) {
+        super(lx);
+    }
+
+    public void run(double deltaMs) {
+
+    }
 }
